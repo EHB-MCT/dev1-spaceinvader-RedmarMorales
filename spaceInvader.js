f1c950fdb88b328620b633559bdb010565c9a5bb
Redmar Morales <redmarfrancis@yahoo.com>
2022-10-05 22:17:17 +0200

    Taak Klaar Morales

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,27 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+drawSpaceinvader();
+
+function drawSpaceinvader() {
+    //background
+    context.beginPath();
+    context.rect(50, 50, 300, 300);
+    context.fill();
+
+    //spaceinvader
+    context.fillStyle = '#66FF00';
+    context.beginPath();
+    context.rect(75, 80, 50, 80);
+    context.rect(175, 80, 50, 130);
+    context.rect(275, 80, 50, 80);
+    context.rect(125, 120, 50, 130);
+    context.rect(225, 120, 50, 130);
+    context.rect(75, 210, 50, 80);
+    context.rect(275, 210, 50, 80);
+    context.fill();
+}
